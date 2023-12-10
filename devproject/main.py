3a6982923bcce405120b8b503dbd35fc8ea4943d
taterowney <51540870+taterowney@users.noreply.github.com>
2023-12-10 10:05:20 -0800

    Delete devproject/main.py

--- devproject/main.py
+++
@@ -1,8 +0,0 @@
-from ElectricPython import sendJSRequest as js
-from ElectricPython import FRONTEND, BACKEND, Window
-
-print(js('console.log("Python is running...")', env=BACKEND))
-
-w = Window(html="<head><title>Test</title></head><body><h1>Test</h1></body>")
-
-print(js('console.log("Python complete!")', env=BACKEND))
